5de38aa3ba2bbc2df1400f3b39122a4a75b5152f
cerXXXX <vma091416@outlook.com>
2025-05-09 20:00:53 +0300

    update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
@@ -12,10 +15,14 @@ class User(db.Model, UserMixin):
 
 
 class Game(db.Model):
+    __tablename__ = 'game'
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    date = db.Column(db.DateTime)
     score = db.Column(db.Integer)
+
+    user = relationship('User', backref='game')
 
 
 class Round(db.Model):
